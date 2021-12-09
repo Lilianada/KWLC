9d9d9c667235195a23b88e4e75102f2fefd6f208
Lilianada <lilibest30.lb@gmail.com>
2021-12-09 20:53:38 +0100

    message

--- script.js
+++ script.js
@@ -72,21 +72,83 @@ class myHeader extends HTMLElement{
 customElements.define('my-header', myHeader)
 
 
-//Dropdown Menu
-// function myFunction() {
-//     document.getElementById("dropdown-contents").classList.toggle("show");
-// }
+class myFooter extends HTMLElement{
+    connectedCallback(){
+        this.innerHTML= `
 
-// Close the dropdown menu if the user clicks outside of it
-// window.onclick = function(event) {
-//     if (!event.target.matches('.dropdown')) {
-//       var dropdowns = document.getElementsByClassName("dropdown_contents");
-//       var i;
-//       for (i = 0; i < dropdowns.length; i++) {
-//         var openDropdown = dropdowns[i];
-//         if (openDropdown.classList.contains('show')) {
-//           openDropdown.classList.remove('show');
-//         }
-//       }
-//     }
-//   }
+        <footer class="footer_container">
+            <div class="footer_col col_address">
+                <div class="footer_header logo_text">
+                    <div class="footer_logo"><img src="images/KWLCLogo.svg" alt="KWLC Logo"></div>
+                    <h5 class="section_subtitle">Kingdom Ways Living Church</h5>
+                </div>
+                <div class="contact_info">
+                    <div class="contact_row">
+                        <label class="label_icon"><i class="fa fa-phone" aria-hidden="true"></i></label>
+                        <p class="contact_text">+ 234 70 433 2832</p> 
+                    </div>
+                
+                    <div class="contact_row">
+                        <label class="label_icon"><i class="fa fa-map-marker" aria-hidden="true"></i> </label>
+                        <p class="contact_text">24 Prince Ibrahim Eletu Avenue, Shoprite Circle Mall Road Jakande Bus Stop, Osapa London,Lagos</p>
+                    </div>
+                
+                    <div class="contact_row">
+                        <label class="label_icon"><i class="fa fa-envelope" aria-hidden="true"></i></label>
+                        <p class="contact_text">info@kwlchq.org</p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="footer_col">
+                <div class="footer_header">
+                    <h4 class="section_title">Contact Us</h4>
+                </div>
+                <div class="contact_info">
+                <form action="">
+                    <input type="text" placeholder="Name" id="">
+                    <input type="email" placeholder="Email" id="">
+                    <textarea placeholder="Message" id="" cols="30" rows="10"></textarea>
+                </form>
+                <button class="button">Send</button>
+                </div>
+            </div>
+
+            <div class="footer_col">
+                <div class="footer_header">
+                    <h4 class="section_title">Up coming Events</h4>
+                </div>
+
+                <div class="contact_col">
+                <h4>Weeding</h4>
+                <span class="contact_span">July 7 @ 8:00 am - 10:30 am</span>
+                </div>
+                <div class="contact_col">
+                    <h4>Weeding</h4>
+                    <span class="contact_span">July 7 @ 8:00 am - 10:30 am</span>
+                </div>
+                <div class="contact_col">
+                    <h4>Weeding</h4>
+                    <span class="contact_span">July 7 @ 8:00 am - 10:30 am</span>
+                </div>
+            </div>
+
+            <div class="footer_col">
+                <div class="footer_header">
+                    <h4 class="section_title">Follow Us</h4>
+                </div>
+                <div class="contact_info txt_icons">
+                <a href="#"><i class="fa fa-linkedin" aria-hidden="true"></i></a>
+                <a href="#"><i class="fa fa-twitter" aria-hidden="true"></i></a>
+                <a href="#"><i class="fa fa-facebook" aria-hidden="true"></i></a>
+                <a href="#"><i class="fa fa-instagram" aria-hidden="true"></i></p>
+                </div>
+            </div>
+            
+        </footer>
+        `
+    }
+}
+
+customElements.define('my-footer', myFooter)
+
