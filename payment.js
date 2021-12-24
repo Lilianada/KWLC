42feb835148e628faa866f9799e577c08cda24fa
Lilianada <lilibest30.lb@gmail.com>
2021-12-24 20:47:28 +0100

    message

---
+++ payment.js
@@ -0,0 +1,47 @@
+//   PAYMENT.HTML SCRIPT
+const btn = document.getElementById('donate');
+const closeBtn = document.getElementById('close');
+const closeBtnModal = document.getElementById('close-modal');
+const modal = document.getElementById('modal');
+const overlay = document.getElementById('overlay');
+const successModal = document.getElementById('success__mod  al');
+const submitDonation = document.getElementById('submit-donation');
+const mainContent = document.getElementById('main-content');
+
+//form
+const form = document.getElementById('form');
+const payForm = document.getElementById('pay-form');
+
+// show Modal
+btn.addEventListener('click', () => {
+    modal.style.display = "block"
+    overlay.style.display = "block"
+});
+
+// hide Modal
+closeBtn.addEventListener('click', () => {
+    modal.style.display = "none"
+    overlay.style.display = "none"
+});
+
+// success Modal
+submitDonation.addEventListener('click', () => {
+    mainContent.style.display = "none"
+    successModal.style.display = "block"
+});
+
+// hide Modal using continue button
+closeBtnModal.addEventListener('click', () => {
+    modal.style.display = "none"
+    overlay.style.display = "none"
+    mainContent.style.display = "block"
+    successModal.style.display = "none"
+})
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+})
+
+payForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+})
--- script.js
+++ script.js
@@ -149,6 +149,7 @@ class myFooter extends HTMLElement{
         `
     }
 }
+
 customElements.define('my-footer', myFooter)
 
 
@@ -171,94 +172,5 @@ function myFunction() {
     }
   }
 
-//   PAYMENT.HTML SCRIPT
-  const btn = document.getElementById('donate');
-  const closeBtn = document.getElementById('close');
-  const closeBtnModal = document.getElementById('close-modal');
-  const modal = document.getElementById('modal');
-  const overlay = document.getElementById('overlay');
-  const successModal = document.getElementById('success__mod  al');
-  const submitDonation = document.getElementById('submit-donation');
-  const mainContent = document.getElementById('main-content');
-
-  //form
-  const form = document.getElementById('form');
-  const payForm = document.getElementById('pay-form');
-
-  // show Modal
-  btn.addEventListener('click', () => {
-      modal.style.display = "block"
-      overlay.style.display = "block"
-  });
-
-  // hide Modal
-  closeBtn.addEventListener('click', () => {
-      modal.style.display = "none"
-      overlay.style.display = "none"
-  });
-
-  // success Modal
-  submitDonation.addEventListener('click', () => {
-      mainContent.style.display = "none"
-      successModal.style.display = "block"
-  });
-
-  // hide Modal using continue button
-  closeBtnModal.addEventListener('click', () => {
-      modal.style.display = "none"
-      overlay.style.display = "none"
-      mainContent.style.display = "block"
-      successModal.style.display = "none"
-  })
-
-  form.addEventListener('submit', (e) => {
-      e.preventDefault();
-  })
-
-  payForm.addEventListener('submit', (e) => {
-      e.preventDefault();
-  })
 
 
-//   DONATE.HTML SCRIPT
-const btn = document.getElementById('donate');
-const closeBtn = document.getElementById('close');
-const closeBtnModal = document.getElementById('close-modal');
-const modal = document.getElementById('modal');
-const overlay = document.getElementById('overlay');
-const successModal = document.getElementById('success__modal');
-const submitDonation = document.getElementById('submit-donation');
-const mainContent = document.getElementById('main-content');
-
-//form
-const form = document.getElementById('form');
-
-// show Modal
-btn.addEventListener('click', () => {
-    modal.style.display = "block"
-    overlay.style.display = "block"
-});
-
-// hide Modal
-closeBtn.addEventListener('click', () => {
-    modal.style.display = "none"
-    overlay.style.display = "none"
-});
-
-// success Modal
-submitDonation.addEventListener('click', () => {
-    mainContent.style.display = "none"
-    successModal.style.display = "block"
-});
-
-// hide Modal using continue button
-closeBtnModal.addEventListener('click', () => {
-    modal.style.display = "none"
-    overlay.style.display = "none"
-    mainContent.style.display = "block"
-    successModal.style.display = "none"
-})
-
-form.addEventListener('submit', (e) => {
-    e.preventDefault();
-})
